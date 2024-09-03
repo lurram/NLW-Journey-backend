5771a45853ac25a700fb77106312f19eb845b0f1
lurram <lurramsantos.00.ls@gmail.com>
2024-09-03 10:58:00 -0300

    first commit NLW - Journet backend

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import cors from '@fastify/cors';
+import fastify from 'fastify';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { confirmParticipant } from './routes/confirm-participant';
+import { confirmTrip } from './routes/confirm-trip';
+import { createActivity } from './routes/create-activity';
+import { createInvite } from './routes/create-invite';
+import { createLink } from './routes/create-link';
+import { createTrip } from './routes/create-trip';
+import { getActivities } from './routes/get-activities';
+import { getLinks } from './routes/get-links';
+import { getParticipant } from './routes/get-participant';
+import { getParticipants } from './routes/get-participants';
+import { getTripDetails } from './routes/get-trip-details';
+import { updateTrip } from './routes/update-trip';
+import { errorHandler } from './error-handler';
+import { env } from './env';
+
+const app = fastify()
+
+app.register(cors, {
+  origin: '*'
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler(errorHandler)
+
+app.register(confirmParticipant)
+app.register(confirmTrip)
+app.register(createActivity)
+app.register(createInvite)
+app.register(createLink)
+app.register(createTrip)
+app.register(getActivities)
+app.register(getLinks)
+app.register(getParticipant)
+app.register(getParticipants)
+app.register(getTripDetails)
+app.register(updateTrip)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('Server Running');
+  
+})
